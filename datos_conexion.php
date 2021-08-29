791650c41fe20cb52b6528419f3197fd34c9539f
Jhensser Aguirre <aguirrejhensser@gmail.com>
2021-08-29 15:13:53 -0600

    Subiendo datos de Conexion

---
+++ datos_conexion.php
@@ -0,0 +1,8 @@
+<!-- Datos de Conexion a Base de Datos MySql-->
+<?php 
+    $db_host = "localhost";
+    $db_usr = "usr_empresa";
+    $db_pass = "Empresa@123";
+    $db_name = "db_empresa_2021";
+    $db_port = "3308";
+?>
